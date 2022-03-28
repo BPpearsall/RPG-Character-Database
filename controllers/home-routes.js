da0f27c33b542eb6640843b55222aad150b773ff
Lyere <BPpearsall@gmail.com>
2022-03-28 15:24:41 -0400

    completed Home Routes controller for review

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -11,9 +11,9 @@ router.get('/', async (req, res) => {
                     attributes: ['name']
                 },
             ],
-        }),
+        });
 
-        const characters = projectData.map((character) => character.get({ plain: true}));
+        const characters = characterData.map((character) => character.get({ plain: true}));
 
         res.render('homepage', {
             characters
@@ -23,4 +23,47 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get()
+router.get('/character/:id', withAuth, async (req, res) => {
+    try {
+        const characterData = await Character.findByPk(req.session.id)
+
+        const character = characterData.get({ plain: true });
+
+        res.render('character', {
+            ...character,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        res.status(400).json(err)
+    }
+});
+
+router.get('/profile', withAuth, async (req, res) => {
+    try {
+      // Find the logged in user based on the session ID
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: Project }],
+      });
+  
+      const user = userData.get({ plain: true });
+  
+      res.render('profile', {
+        ...user,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/homepage');
+      return;
+    }
+  
+    res.render('login');
+  });
--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
+const createCharacter = require('./create-character')
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/create-character', createCharacter)
 
 module.exports = router;
