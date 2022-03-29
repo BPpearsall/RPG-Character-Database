d5cc6c4fc63a9170fbb37201f42d7db31d1b39fd
jsnyder159 <jw.snyder159@gmail.com>
2022-03-29 19:39:47 -0400

    created comment api routes

---
+++ controllers/api/post-comment.js
@@ -0,0 +1,53 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+
+router.get('/id', async (req, res) => {
+    try {
+        const commentData = await Comment.findByPk({
+            where: {
+                id: req.params.id,
+            }})
+            const comment = commentData.get({ plain: true });
+            res.render('comment', {
+                comment,
+            })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err)
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const newComment = await Comment.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+
+        res.status(200).json(newComment);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const commentData = await Comment.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+        
+        if(!commentData) {
+            res.status(404).json({ message: "No comment found with this id."});
+            return;
+        }
+
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
