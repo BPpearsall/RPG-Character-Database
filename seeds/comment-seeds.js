7f50a5108c8be33c45a987334111d08db7d1ac9b
Jason Snyder <jason.snyder159@gmail.com>
2022-03-29 12:57:41 -0400

    made alot of seeds for character, class, commment, and users.

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -4,8 +4,68 @@ const commentData = [
     {
         body: 'Very Nice',
         character_id: 1,
+        user_id: 2,
+    },
+    {
+        body: 'Awful luck with the hand.',
+        character_id: 2,
+        user_id: 3,
+    },
+    {
+        body: 'Good stuff.',
+        character_id: 3,
+        user_id: 2,
+    },
+    {
+        body: 'Very cute and scary little rabbit.',
+        character_id: 4,
+        user_id: 3,
+    },
+    {
+        body: 'Does he have friends that are a rabbit, a pig, and a bear who loves honey?',
+        character_id: 5,
         user_id: 1,
-    }
+    },
+    {
+        body: 'A FRIENDLY GHOST!?',
+        character_id: 6,
+        user_id: 3,
+    },
+    {
+        body: 'Super sailor.',
+        character_id: 7,
+        user_id: 3,
+    },
+    {
+        body: 'So mean!',
+        character_id: 8,
+        user_id: 1,
+    },
+    {
+        body: 'Really good character!',
+        character_id: 9,
+        user_id: 2,
+    },
+    {
+        body: 'Does he juggle?',
+        character_id: 10,
+        user_id: 1,
+    },
+    {
+        body: 'Does he have the machine gun blues?',
+        character_id: 11,
+        user_id: 2,
+    },
+    {
+        body: 'Does he love to sing the DOOM song?',
+        character_id: 12,
+        user_id: 1,
+    },
+    {
+        body: 'Very Nice!',
+        character_id: 12,
+        user_id: 2,
+    },
 ];
 
 const seedComment = () => Comment.bulkCreate(commentData);
--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -5,7 +5,17 @@ const userData = [
         name: 'Donkey',
         email: "thatlldo@gmail.com",
         password: "abcd1234",
-    }
+    },
+    {
+        name: 'FriarTuck',
+        email: 'monk@gmail.com',
+        password: 'abcd1234',
+    },
+    {
+        name: 'EdEDDeddie',
+        email: 'best3brothers@gmail.com',
+        password: 'abcd1234',
+    },
 ];
 
 const seedUser = () => User.bulkCreate(userData);
