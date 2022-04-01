ec30f6b1eca47428d78a679406f0ce61a4f377e1
Lyere <BPpearsall@gmail.com>
2022-04-01 10:28:09 -0400

    created functional signup routes for new users

--- public/js/signup.js
+++ public/js/signup.js
@@ -6,7 +6,7 @@ const signupFormHandler = async (event) => {
     const password = document.querySelector('#password-signup').value.trim();
   
     if (name && email && password) {
-      const response = await fetch('/api/user', {
+      const response = await fetch('/api/user/signup', {
         method: 'POST',
         body: JSON.stringify({ name, email, password }),
         headers: { 'Content-Type': 'application/json' },
